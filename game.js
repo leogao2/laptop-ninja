0f7a55000e3f4c3933de1edf1d8744f515d559bc
Leo Gao <leogao31@gmail.com>
2020-01-18 14:13:25 -0700

    Switch to instance

--- game.js
+++ game.js
@@ -1,19 +1,3 @@
-const s = ( sketch ) => {
-
-  let x = 100;
-  let y = 100;
-
-  sketch.setup = () => {
-    sketch.createCanvas(640, 480);
-  };
-
-  sketch.draw = () => {
-    sketch.background(0);
-    sketch.fill(255);
-    sketch.rect(x,y,50,50);
-  };
-};
-
 
 
 var ballX = 300;
@@ -24,11 +8,11 @@ var vY = 20;
 var balls = [];
 
 class Ball {
-  constructor() {
-    this.x = Math.random() * width;
-    this.y = height;
+  constructor(sketch) {
+    this.x = Math.random() * sketch.width;
+    this.y = sketch.height;
     this.vY = 20;
-    this.vX = ((this.x > width / 2) ? -1 : 1) * (Math.random()) * 5;
+    this.vX = ((this.x > sketch.width / 2) ? -1 : 1) * (Math.random()) * 5;
     this.aY = -0.5;
   }
   update() {
@@ -38,23 +22,33 @@ class Ball {
   }
 }
 
-function draw() {
-  background(255);
+const s = (sketch) => {
 
-  fill(0);
-  textSize(30);
-  textAlign(LEFT, TOP);
-  text('Score: ', 10, 10);
-  for (let i = 0; i < balls.length; i++) {
-    let ball = balls[i];
-    ball.update();
-    ellipse(ball.x, ball.y, 80, 80);
-  }
+  sketch.setup = () => {
+    sketch.createCanvas(640, 480);
+  };
 
-  if (frameCount % 100 == 0) {
-      balls.push(new Ball())
+  sketch.draw = () => {
+    sketch.background(255);
+  
+    sketch.fill(0);
+    sketch.textSize(30);
+    sketch.textAlign(sketch.LEFT, sketch.TOP);
+    sketch.text('Score: ', 10, 10);
+    for (let i = 0; i < balls.length; i++) {
+      let ball = balls[i];
+      ball.update();
+      sketch.ellipse(ball.x, ball.y, 80, 80);
+    }
+  
+    if (sketch.frameCount % 100 == 0) {
+        balls.push(new Ball(sketch))
+    }
+    // Create mouse trail
+    sketch.fill(255, 0, 0);
+    sketch.ellipse(sketch.mouseX, sketch.mouseY, 10, 10);
   }
-  // Create mouse trail
-  fill(255, 0, 0);
-  ellipse(mouseX, mouseY, 10, 10);
-}
+  
+};
+
+let myp5 = new p5(s);
